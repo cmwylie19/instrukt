3c222b5a03d91ab25075781e8737394e9289841a
Case Wylie <cmwylie19@defenseunicorns.com>
2024-10-10 12:44:30 -0400

    chore: add ZarfPackage
    Signed-off-by: Case Wylie <cmwylie19@defenseunicorns.com>

--- capabilities/controller.ts
+++ capabilities/controller.ts
@@ -1,8 +1,4 @@
-import {
-  Capability,
-  a,
-  sdk
-} from "pepr";
+import { Capability, a, sdk } from "pepr";
 import { WebApp } from "./webapp-v1alpha1";
 import Deploy from "./deployer";
 
@@ -13,48 +9,49 @@ export const Controller = new Capability({
   description: "Control Apps and Global Cluster Security Posture.",
 });
 
-
 const { When } = Controller;
 
 // Validate Pods are not running with privileged security context
 When(a.Pod)
-.IsCreatedOrUpdated()
-.Validate(po => {
+  .IsCreatedOrUpdated()
+  .Validate(po => {
     const podContainers = containers(po);
 
     for (const container of podContainers) {
-        if (container.securityContext?.privileged) {
-            return po.Deny("Pods cannot run with privileged security context");
-        }
+      if (container.securityContext?.privileged) {
+        return po.Deny("Pods cannot run with privileged security context");
+      }
     }
     return po.Approve();
-})
+  });
 
 // Assign sane defaults for pods
 When(a.Pod)
-.IsCreatedOrUpdated()
-.Mutate(po => {
+  .IsCreatedOrUpdated()
+  .Mutate(po => {
     const podContainers = containers(po);
 
     for (const [index, container] of podContainers.entries()) {
-        if (!container.resources || !container.resources.requests && !container.resources.limits) {
-
-            po.Raw.spec.containers[index].resources = {
-                requests: {
-                    cpu: "100m",
-                    memory: "128Mi",
-                },
-                limits: {
-                    cpu: "500m",
-                    memory: "512Mi",
-                },
-            };
-        }
+      if (
+        !container.resources ||
+        (!container.resources.requests && !container.resources.limits)
+      ) {
+        po.Raw.spec.containers[index].resources = {
+          requests: {
+            cpu: "100m",
+            memory: "128Mi",
+          },
+          limits: {
+            cpu: "500m",
+            memory: "512Mi",
+          },
+        };
+      }
     }
-})
+  });
 
 When(WebApp)
-.IsCreatedOrUpdated()
-.Reconcile(async webapp => {
+  .IsCreatedOrUpdated()
+  .Reconcile(async webapp => {
     await Deploy(webapp);
-})
+  });
